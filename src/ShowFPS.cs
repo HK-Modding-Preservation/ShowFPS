c42b9f654b9ca54acfe7036426af3b455e6dcee1
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-07-29 18:27:24 +0200

    why was that there

--- src/ShowFPS.cs
+++ src/ShowFPS.cs
@@ -13,19 +13,6 @@ namespace ShowFPS
 
         public override void Initialize()
         {
-            Vector2 test2Orig = new Vector2(1f, 1f);
-            Vector3 test3Orig = new Vector3(1f, 1f, 1f);
-            Vector4 test4Orig = new Vector4(1f, 1f, 1f, 1f);
-
-            Vector2 test2From2 = test2Orig;
-            Vector2 test2From3 = test3Orig;
-            Vector2 test2From4 = test4Orig;
-            Vector3 test3From2 = test2Orig;
-            Vector3 test3From3 = test3Orig;
-            Vector3 test3From4 = test4Orig;
-            Vector4 test4From2 = test2Orig;
-            Vector4 test4From3 = test3Orig;
-            Vector4 test4From4 = test4Orig;
         }
     }
 }
