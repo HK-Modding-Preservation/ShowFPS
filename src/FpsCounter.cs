e319cfd4914aad06804bc49289400b5da393c427
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-07-29 18:12:37 +0200

    initial commit

---
+++ src/FpsCounter.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Logger = Modding.Logger;
+
+namespace ShowFPS;
+
+public class FpsCounter : MonoBehaviour
+{
+    private const float FpsUpdateInterval = 0.5f;
+
+    private double _currentFps = -1.0;
+    private double _averageFps = -1.0;
+    private double _maxFps = -1.0;
+    private double _minFps = -1.0;
+    private UInt32 _fpsCounter = 0;
+    private UInt32 _cycleCounter = 0;
+    private string _displayText = "";
+    private readonly GUIStyle _textStyle = new GUIStyle(GUIStyle.none);
+    private Font _monoFont;
+
+    private void Start()
+    {
+        _monoFont = Font.CreateDynamicFontFromOSFont(new string[]
+        {
+            // Windows
+            "Consolas",
+            // Mac
+            "Menlo",
+            // Linux
+            "Courier New",
+            "DejaVu Mono"
+        }, 15);
+        _textStyle.font = _monoFont;
+        _textStyle.normal.textColor = Color.white;
+        _textStyle.alignment = TextAnchor.LowerRight;
+        _textStyle.padding = new RectOffset(5, 5, 5, 5);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(CalcTimedFps());
+    }
+
+    private IEnumerator CalcTimedFps()
+    {
+        while (isActiveAndEnabled)
+        {
+            yield return new WaitForSecondsRealtime(FpsUpdateInterval);
+
+            ++_cycleCounter;
+
+            _currentFps = _fpsCounter / FpsUpdateInterval;
+            _averageFps = _averageFps < 0 ? _currentFps : Average(_averageFps, _currentFps, _cycleCounter);
+            _maxFps = _maxFps < 0 ? _currentFps : Max(_maxFps, _currentFps);
+            _minFps = _minFps < 0 ? _currentFps : Min(_minFps, _currentFps);
+
+            GenerateText();
+
+            _fpsCounter = 0;
+        }
+    }
+
+    private void Update()
+    {
+        ++_fpsCounter;
+    }
+
+    private void GenerateText()
+    {
+        _displayText = FormatRow("FPS:", "");
+        _displayText += "\n" + FormatRow("Current:", _currentFps);
+        _displayText += "\n" + FormatRow("Average:", _averageFps);
+        _displayText += "\n" + FormatRow("Max:", _maxFps);
+        _displayText += "\n" + FormatRow("Min:", _minFps);
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), _displayText, _textStyle);
+    }
+
+    private const string formatName = "{0,-8}";
+    private const string formatValueDouble = "{0,7:F1}";
+    private const string formatValueString = "{0,7}";
+
+    private string FormatRow(string name, double value)
+    {
+        return String.Format(CultureInfo.InvariantCulture, formatName, name) + " " + String.Format(CultureInfo.InvariantCulture, formatValueDouble, value);
+    }
+    private string FormatRow(string name, string value)
+    {
+        return String.Format(CultureInfo.InvariantCulture, formatName, name) + " " + String.Format(CultureInfo.InvariantCulture, formatValueString, value);
+    }
+
+    public static double Average(double average, double fpsFrame, double frameNumber)
+    {
+        return ((average * (frameNumber - 1)) + fpsFrame) / frameNumber;
+    }
+
+    public static double Max(double max, double fpsFrame)
+    {
+        return max < fpsFrame ? fpsFrame : max;
+    }
+
+    public static double Min(double min, double fpsFrame)
+    {
+        return min < fpsFrame ? min : fpsFrame;
+    }
+}
---
+++ src/ShowFPS.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using Modding;
+using UnityEngine;
+
+namespace ShowFPS
+{
+    public class ShowFPS : Mod
+    {
+        public ShowFPS() : base("Show FPS")
+        {
+            GameObject go = new GameObject("fps counter", typeof(FpsCounter));
+            GameObject.DontDestroyOnLoad(go);
+
+            ModHooks.ApplicationQuitHook += () =>
+            {
+                //var counter = go.GetComponent<FpsCounter>();
+                //foreach (var fps in counter.GetFpsNumbers())
+                //{
+                //    Log("[FPS] - " + fps.ToString("F1", CultureInfo.InvariantCulture));
+                //}
+            };
+        }
+
+        public override void Initialize()
+        {
+            Vector2 test2Orig = new Vector2(1f, 1f);
+            Vector3 test3Orig = new Vector3(1f, 1f, 1f);
+            Vector4 test4Orig = new Vector4(1f, 1f, 1f, 1f);
+
+            Vector2 test2From2 = test2Orig;
+            Vector2 test2From3 = test3Orig;
+            Vector2 test2From4 = test4Orig;
+            Vector3 test3From2 = test2Orig;
+            Vector3 test3From3 = test3Orig;
+            Vector3 test3From4 = test4Orig;
+            Vector4 test4From2 = test2Orig;
+            Vector4 test4From3 = test3Orig;
+            Vector4 test4From4 = test4Orig;
+        }
+    }
+}
