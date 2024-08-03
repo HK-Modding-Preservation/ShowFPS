b256c84f29218ef9da2f72e28a97868031b23c62
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-08-03 21:30:50 +0200

    file-scoped namespaces fix csproj

--- src/ShowFPS.cs
+++ src/ShowFPS.cs
@@ -1,18 +1,17 @@
 ﻿using Modding;
 using UnityEngine;
 
-namespace ShowFPS
-{
-    public class ShowFPS : Mod
-    {
-        public ShowFPS() : base("Show FPS")
-        {
-            GameObject go = new GameObject("fps counter", typeof(FpsCounter));
-            GameObject.DontDestroyOnLoad(go);
-        }
+namespace ShowFPS;
 
-        public override void Initialize()
-        {
-        }
+public class ShowFPS : Mod
+{
+    public ShowFPS() : base("Show FPS")
+    {
+        GameObject go = new GameObject("fps counter", typeof(FpsCounter));
+        GameObject.DontDestroyOnLoad(go);
+    }
+
+    public override void Initialize()
+    {
     }
 }
